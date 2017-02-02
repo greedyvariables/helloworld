8048b33c8f6d4cf5f09a15e42c8abf8351ad0d45
greedyvariables <nidzg1md@gmail.com>
2017-02-01 19:51:49 -0500

    Update default.rb

--- default.rb
+++ default.rb
@@ -1,22 +1,14 @@
-################################################################################
-# Licensed Materials - Property of IBM Copyright IBM Corporation 2015. All Rights Reserved.
-# U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP
-# Schedule Contract with IBM Corp.
-################################################################################
-#
-#updated
 
 #Test Comment
 
-case node[:platform_family]
-when 'rhel'
-	default[:helloworld][:file] = "/tmp/helloworld.txt"
-	default[:helloworld][:content] = "It works!"
-  default[:helloworld][:os_user] = "hw_user"
-  default[:helloworld][:os_password] = "Just4Now"  
-  default[:helloworld][:os_group] = "hw_group"
-  default[:helloworld][:secondary_groups] = []
+case 
+when node[:platform_family] == 'rhel'
+  default[:helloworld][:file] = "/tmp/helloworld.txt"
+  default[:helloworld][:content] = "It works!"
+  #default[:helloworld][:os_user] = "hw_user"
+  #default[:helloworld][:os_password] = "Just4Now"  
+  #default[:helloworld][:os_group] = "hw_group"
 when 'windows'
-	default[:helloworld][:file] = "C:\helloworld.txt"
-	default[:helloworld][:content] = "It works!"  
+  default[:helloworld][:file] = "C:\helloworld.txt"
+  default[:helloworld][:content] = "It works!"  
 end
