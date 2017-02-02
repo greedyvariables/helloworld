25e73da7f9e91f9326b52616ac6f88b8d83b955f
greedyvariables <nidzg1md@gmail.com>
2017-02-01 20:03:32 -0500

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "helloworld"
+license          "Apache 2.0"
+license          "All rights reserved"
+description      "helloworld"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+depends "test-helper"
